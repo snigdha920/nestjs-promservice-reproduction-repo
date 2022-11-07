b16bb63d4f2cdfaab90f2acc51d306c8dcf560ac
Snigdha Singh <62167899+snigdha920@users.noreply.github.com>
2022-11-07 11:58:43 +0100

    inject PromService in app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { PromModule } from '@digikare/nestjs-prom';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [PromModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { PromService } from '@digikare/nestjs-prom';
+import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  @Inject(PromService) promServiceProperyInjection!: PromService;
+
+  constructor(private readonly promServiceConstructorInjection: PromService) {
+    console.log(
+      'promService inside AppService constructor - ',
+      this.promServiceProperyInjection,
+    );
+    console.log(
+      'promServiceConstructorInjection inside AppService constructor - ',
+      this.promServiceConstructorInjection,
+    );
+  }
   getHello(): string {
     return 'Hello World!';
   }
